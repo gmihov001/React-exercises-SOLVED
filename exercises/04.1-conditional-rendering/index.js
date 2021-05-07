a85bc9e0e093a49f91604dd47bb7a4e5f2730a37
George Mihov <gmihov001@gmail.com>
2021-05-07 21:54:22 +0000

    4.1 solved

--- exercises/04.1-conditional-rendering/index.js
+++ exercises/04.1-conditional-rendering/index.js
@@ -3,9 +3,20 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 const Alert = props => {
-	//your component here
+	const colorClasses = {
+		red: "alert-danger",
+		orange: "alert-warning"
+	};
 
+	return (
+		<>
+			<div class={`alert ${colorClasses[props.color]}`} role="alert">
+				{props.text}
+			</div>
+		</>
+	);
 };
+
 Alert.propTypes = {
 	color: PropTypes.string,
 	text: PropTypes.string
